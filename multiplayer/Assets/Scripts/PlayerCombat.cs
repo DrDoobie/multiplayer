1cc1450dacf4e2a725eb339be9c143156206a3c6
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-25 00:19:12 +0200

    added goal model

---
+++ multiplayer/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerCombat : NetworkBehaviour
+{
+    [Header("Animation")]
+    public bool isAnimated;
+    public Animator animator;
+    public NetworkAnimator networkAnimator;
+
+    void Update()
+    {
+        if(isLocalPlayer)
+        {
+            if(isAnimated)
+            {
+                if(Input.GetButtonDown("Fire1"))
+                {
+                    //animator.SetTrigger("Punch");
+                    networkAnimator.SetTrigger("Punch");
+                }
+            }
+        }
+    }
+}
--- multiplayer/Assets/Scripts/PlayerMovement.cs
+++ multiplayer/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ using Mirror;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    public float moveSpeed = 12.0f, jumpHeight = 3.0f, gravityForce = -9.81f;
+    public bool isRunning = false;
+    public float walkSpeed = 12.0f, runSpeed = 16.0f, jumpHeight = 3.0f, gravityForce = -9.81f;
     public Camera cam;
     public CharacterController controller;
     public Transform groundCheck;
@@ -14,8 +15,10 @@ public class PlayerMovement : NetworkBehaviour
     [Header("Animation")]
     public bool isAnimated;
     public Animator animator;
+    public NetworkAnimator networkAnimator;
 
     bool isGrounded;
+    float moveSpeed;
     [SerializeField] float groundCheckRadius = 0.4f;
     Vector3 currentVelocity;
 
@@ -52,15 +55,47 @@ public class PlayerMovement : NetworkBehaviour
         {
             animator.SetBool("isWalking", movePos.magnitude > 0.1f ? true : false);
 
-            animator.SetBool("isIdle", movePos.magnitude == 0.0f ? true : false);
+            animator.SetBool("isIdle", movePos.magnitude == 0.0f ? true : false);  
         }
 
+        //Running
+        if(Input.GetButton("Run"))
+        {
+            isRunning = true;
+
+        } else {
+            isRunning = false;
+        }
+
+        if(isRunning)
+        {
+            moveSpeed = runSpeed;
+
+        } else {
+            moveSpeed = walkSpeed;
+        }
+
+        //Move
         controller.Move(movePos * moveSpeed * Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        //Jumping
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            currentVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityForce);
+            if(isAnimated)
+            {
+                //animator.SetTrigger("Jump");
+                networkAnimator.SetTrigger("Jump");
+
+                return;
+            }
+
+            Jump();
         }
+    }
+
+    public void Jump()
+    {
+        currentVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityForce);
     }
 
     void Gravity()
